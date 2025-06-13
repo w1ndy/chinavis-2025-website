b63ba9c27970e1b5f8e25e227503c5234129e9a7
Di Weng <dweng.cs@outlook.com>
2025-06-13 15:03:39 +0800

    add exhibition

---
+++ src/i18n/exhibition.ts
@@ -0,0 +1,127 @@
+import { marked } from "marked";
+
+export const zh = {
+  PageTitle: "参展 - ChinaVis 2025",
+  Title: "第十二届中国可视化与可视分析大会（ChinaVis 2025）<br />参展",
+  Content: marked.parse(
+    `
+    ## 参展邀请
+
+    第十二届中国可视化与可视分析大会（ChinaVis 2025）将于 2025 年 7 月 19 日 - 22 日在中国杭州举行。为了进一步推动产学研的交流合作，大会期间将组织可视化技术与产品展览，诚邀广大高校、研究机构和行业领先公司参加ChinaVis 2025展览！
+
+    ## 参展价值
+
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+    <img src="/2025/exhibits/value1.png" alt="参展价值1" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+    中国可视化与可视分析大会 (ChinaVis) 由我国可视化业界工作者联合发起，宗旨是促进中国及周边地区的可视化与可视分析研究与应用的交流，探讨在大数据时代可视化与可视分析发展的方向与机遇，推动相关研究与应用的发展与进步，推进学科的发展，促进人才培养和交流，加深艺术与技术融合。会议将搭建与国内外著名专家、企业家、应用部门面对面交流，深入研讨可视化前沿技术及其应用的交流和沟通平台，促进中国可视化与可视分析领域产、学、研、用协同发展新生态的形成；
+    </p>
+
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+      <img src="/2025/exhibits/value2.png" alt="参展价值2" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+      ChinaVis 展览能够让参会人员直观感受参展单位的最新的研究成果和产品。提供更多机会与潜在研究合作者或商业客户见面、沟通，为推动科研与商务合作提供机遇；
+    </p>
+
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+      <img src="/2025/exhibits/value3.png" alt="参展价值3" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+      ChinaVis展览能够有效提高参展单位的知名度，提供发现和招募可视化领域研究人才机会。
+    </p>
+
+    ## 展览信息
+
+    ### 展览时间与地点
+
+    - 展览时间：2025 年 7 月 19 日至 22 日
+    - 展览地点：杭州城北文澜大酒店
+    - 展览申请截止日期：2025 年 6 月 22 日
+
+
+    ### 展览空间平面图
+
+    待定
+
+    ### 展位
+
+    每个展位为 1.5×2 米标准展位，提供一张桌子、两把折椅、一台显示器、220V/10A 电源插座。每个单位可以申请至多不超过 3 个展位。展位分配按照先申请先获得原则。
+
+    ### 参展费用
+
+    - 高校及科研院所：6000元/展位，赠送2个全额注册（1个学生+1个教师）
+    - 企业单位：20000元/展位，赠送2个全额注册。
+
+    注：高校、科研院所、企业也可以通过会议赞助方式获得展位，具体方案联系ChinaVisSponsor@163.com。
+
+    ### 申请参展时间与流程
+
+    1. 有意者请下载填写<a href="/2025/exhibits/application.docx" target="_blank">申请表</a>后发送到表格指定邮箱
+    2. 如申请成功，将收到确认通知邮件。
+    `.replace(/^    /gm, "")
+  ),
+};
+
+export const en = {
+  PageTitle: "Exhibition - ChinaVis 2025",
+  Title:
+    '<span style="font-size: 0.8em">The 12th China Conference on Visualization and Visual Analytics (ChinaVis 2025)</span><br />Exhibition',
+  Content: marked.parse(
+    `
+    ## Invitation to Exhibit
+
+    ChinaVis (2025) will be held in Hangzhou, P.R.China from July 19th to 22nd this year. The ChinaVis2025 exhibits program is an opportunity for communication and cooperation between experts, entrepreneurs and researchers. The exhibition will present the latest related products and technologies in the field. We invite providers from companies, research institutions and universities.
+
+    **Please join us at Hangzhou as a ChinaVis 2025 Exhibitor.**
+
+    ## Reasons for Exhibitors
+
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+      <img src="/2025/exhibits/value1.png" alt="Reason1" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+      ChinaVis builds a communication platform for famous experts, entrepreneurs and application departments at home and abroad to meet face-to-face and further discuss frontier technologies and applications of visualization.
+    </p>
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+      <img src="/2025/exhibits/value2.png" alt="Reason2" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+      Exhibitors showcase new corporate inventions and innovations. The cutting edge of research and technology will be publicly seen. It’s a great way to get opportunities for cooperation.
+    </p>
+    <p style="clear: both; text-indent: 0; margin-bottom: 1em">
+      <img src="/2025/exhibits/value3.png" alt="Reason3" style="width: 50px; height: 50px; float: left; margin-right: 5px" />
+      Recruit the top visualization talent from thousands of participants from diverse backgrounds.
+    </p>
+
+    <br />
+
+    ## About Exhibiting
+
+    ### Location
+
+    Wenlan Hotel
+
+    ### Plan of Booth Space
+
+    TBD
+
+    ### Booth Description
+
+    Space Rental Includes:
+    - 1.5m x 2m Booth Space
+    - 220V/10A Power Supply
+    - 1 Table
+    - 2 Folding Chairs
+    - 1 Display
+
+    At most 3 booth spaces can be rent for one organization.
+
+    Assignments will be made on a “first come, first served” basis.
+
+    ### Booth Fees
+
+    - For university and research institute, RMB 6,000 for one booth space, including 2 free registration (1 teacher, 1 student)
+    - For enterprise, RMB 20,000 for one booth space, including 2 free registration
+
+    Organizations can apply for booth space by the sponsor. Require details, please mail to ChinaVisSponsor@163.com.
+
+    ### How to Apply
+
+    - Download and fill the <a href="/2025/exhibits/application_en.docx" target="_blank">Application Form</a>, then send it to the specified email.
+    - Receive the notification mail of confirmation.
+    - Please apply for booth space before 22nd June, 2025
+    `.replace(/^    /gm, "")
+  ),
+};
