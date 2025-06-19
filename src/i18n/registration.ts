d3ed3320dd226c6b19a6cf833ce1742d1f656ecd
Di Weng <dweng.cs@outlook.com>
2025-06-19 20:42:38 +0800

    update registration page

--- src/i18n/registration.ts
+++ src/i18n/registration.ts
@@ -12,7 +12,7 @@ export const zh = {
 
     会议通知下载： 待公布
 
-    会址及酒店信息： 待公布
+    会址及酒店信息： [ChinaVis 2025 会议地址](/2025/zh/venue)
 
     ## 个人信息注册
 
@@ -108,6 +108,36 @@ export const zh = {
       </table>
     </div>
 
+    ## 会议费支付
+
+    **请在汇款/转账附言里填写"可视化+注册编号"，便于到账金额查询。转账完成后，将转账凭证的电子版（扫描或照片）作为附件，邮件标题为“ChinaVis2025注册费 + 注册编号 + 姓名”，发邮件至 chinavis2025_reg@163.com，** 同时提供以下信息：注册编号，姓名，单位，文章编号（如果有文章）。如需要开增值税发票，请注册时提供单位名称、纳税人识别号及发票内容等信息。 **十人以上团体提前注册有优惠，团体注册每人减免100元，请联系 chinavis2025_reg@163.com 咨询具体支付方式; 暑期学校+会议注册也请联系 chinavis2025_reg@163.com 咨询具体支付方式。**
+
+    <div class="methods">
+      <table>
+        <thead>
+          <tr>
+            <td>支付方式1：银行转账</td>
+            <td>支付方式2：支付宝转账</td>
+            <td>支付方式3：微信转账</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <strong>户　名：</strong>中国图象图形学学会<br />
+              <strong>账　号：</strong>0200049609200073436<br />
+              <strong>开户行：</strong>工行海淀支行
+            </td>
+            <td>
+              支付宝二维码
+            </td>
+            <td>
+              微信二维码
+            </td>
+          </tr>
+      </table>
+    </div>
+
     ## 退费政策
 
     会议举行前 30 日以前申请退费，可免手续费；会议举行前 15-30 日（含）申请退费，扣除 30% 手续费；会议举行前 7-15 日（含）申请退费，扣除 50% 手续费；会议举行前 7 日（含）内申请退费或未参加会议不予退款。
@@ -123,10 +153,156 @@ export const zh = {
     `.replace(/  +/g, "")
   ),
 };
-
 export const en = {
   PageTitle: "Registration - ChinaVis 2025",
   Title:
     '<span style="font-size: 0.8em">The 12th China Conference on Visualization and Visual Analytics (ChinaVis 2025)</span><br />Conference Registration',
-  ToBeAnnounced: "To be announced",
+  Content: marked.parse(
+    `
+    Participants are required to register their personal information online first and learn about the registration fees based on their registration type. Participants can pay through bank transfer, Alipay, or WeChat transfer. Postal remittance is not accepted. Please include "Visualization + Registration Number" in the transfer note for easy tracking of received payments. Student registrants must provide their student ID or other proof of student status during conference check-in.
+
+    Conference Registration Portal: [ChinaVis 2025 Conference Registration](https://meeting.csig.org.cn/exh/default2/baoming?fair_id=339&role_id=22&q=1)
+
+    Conference Notice Download: To be announced
+
+    Venue and Hotel Information: [ChinaVis 2025 Conference Venue](/2025/en/venue)
+
+    ## Personal Information Registration
+
+    Please register your personal information online. The registered information will be used for name tag printing. After successful registration, the system will send a registration number to your registered email address. Please keep your registration number safe, as it will be used for conference check-in and registration fee payment. All attendees **must** register their personal information online.
+
+    ## Registration Fee Types
+
+    Please choose the appropriate registration type based on your situation. Conference papers require at least one author to register as a non-student. "Member" refers to members of the China Society of Image and Graphics (CSIG). Click [here](http://membership.csig.org.cn/ass/apply/step?fair_id=223) to join CSIG. **For non-members, we recommend applying for membership first to enjoy discounted registration fees after obtaining a membership number. When applying for membership, please select the Visualization and Visual Analytics Technical Committee and choose committee member recommendation.**
+
+    Volunteers must be CSIG student members. Volunteers need to register for approval first (http://chinavis.org/2025/en/volunteer), obtain volunteer qualification, and then proceed with registration.
+
+    <div class="registration">
+      <table>
+        <thead>
+          <tr>
+            <td rowspan="2" colspan="2">Registration Type</td>
+            <td colspan="2">Early Registration<br />(Until June 30, 23:59)</td>
+            <td colspan="2">Regular Registration<br />(July 1 - July 19)</td>
+            <td colspan="2">On-site Registration<br />(July 20 - July 22)</td>
+            <td colspan="2">Single-day Registration<br />(Attend one day only)</td>
+          </tr>
+          <tr>
+            <td>Member</td>
+            <td>Non-member</td>
+            <td>Member</td>
+            <td>Non-member</td>
+            <td>Member</td>
+            <td>Non-member</td>
+            <td>Member</td>
+            <td>Non-member</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowspan="2">Student Registration</td>
+            <td>Conference Registration</td>
+            <td>¥1400</td>
+            <td>¥1800</td>
+            <td>¥1800</td>
+            <td>¥2300</td>
+            <td>¥2000</td>
+            <td>¥2500</td>
+            <td>¥1100</td>
+            <td>¥1400</td>
+          </tr>
+          <tr>
+            <td>Summer School + Conference Registration</td>
+            <td>¥1500</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td rowspan="2">Regular Registration</td>
+            <td>Conference Registration</td>
+            <td>¥2200</td>
+            <td>¥2800</td>
+            <td>¥2800</td>
+            <td>¥3500</td>
+            <td>¥3000</td>
+            <td>¥3800</td>
+            <td>¥1200</td>
+            <td>¥1500</td>
+          </tr>
+          <tr>
+            <td>Summer School + Conference Registration</td>
+            <td>¥4200</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+          <tr>
+            <td>Volunteer Registration</td>
+            <td></td>
+            <td>¥700</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    ## Conference Fee Payment
+
+    **Please include "Visualization + Registration Number" in the transfer note for easy tracking of received payments. After completing the transfer, please send an email with the electronic copy of the transfer receipt (scan or photo) as an attachment to chinavis2025_reg@163.com. The email subject should be "ChinaVis2025 Registration Fee + Registration Number + Name".** Please also provide the following information: registration number, name, institution, paper ID (if applicable). If you need a VAT invoice, please provide the company name, taxpayer identification number, and invoice content during registration. **Group registrations of 10 or more people receive early registration discounts with 100 RMB reduction per person. Please contact chinavis2025_reg@163.com for specific payment methods. For Summer School + Conference registration, please also contact chinavis2025_reg@163.com for specific payment methods.**
+
+    <div class="methods">
+      <table>
+        <thead>
+          <tr>
+            <td>Payment Method 1: Bank Transfer</td>
+            <td>Payment Method 2: Alipay Transfer</td>
+            <td>Payment Method 3: WeChat Transfer</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <strong>Account Name:</strong> China Society of Image and Graphics<br />
+              <strong>Account Number:</strong> 0200049609200073436<br />
+              <strong>Bank:</strong> ICBC Haidian Branch
+            </td>
+            <td>
+              Alipay QR Code
+            </td>
+            <td>
+              WeChat QR Code
+            </td>
+          </tr>
+      </table>
+    </div>
+
+    ## Refund Policy
+
+    Refund applications submitted 30 days or more before the conference are free of handling fees; applications submitted 15-30 days (inclusive) before the conference will be charged a 30% handling fee; applications submitted 7-15 days (inclusive) before the conference will be charged a 50% handling fee; applications submitted within 7 days (inclusive) before the conference or non-attendance will not be refunded.
+
+    After refund applications are approved, the issued electronic invoice will be voided and should not be used. Refunds will be processed after the conference, and handling fees will be invoiced as conference service fees.
+
+    ## Important Notes
+
+    1. Each paper must have at least one non-student registration.
+    2. When transferring, be sure to include "Visualization + Registration Number".
+    3. Electronic invoices for conference registration fees will be sent to the registrant's email address. Paper invoices can be collected at the conference venue if needed.
+    4. For any questions regarding registration, please email chinavis2025_reg@163.com.
+    `.replace(/  +/g, "")
+  ),
 };
