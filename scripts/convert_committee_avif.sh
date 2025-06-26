3e409e27a174d0d002454b5f896e52f6796b9bb8
Di Weng <dweng.cs@outlook.com>
2025-06-26 15:50:31 +0800

    fix photo conversion script

--- scripts/convert_committee_avif.sh
+++ scripts/convert_committee_avif.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in src/assets/committee/original/*; do
-  magick $file -resize 256x352^ -gravity center -extent 256x352 "src/assets/committee/$(basename $file | cut -d. -f1).avif"
+for file in original/*; do
+  magick $file -resize '256x352^' -gravity center -extent 256x352 "./$(basename $file | cut -d. -f1).avif"
 done
