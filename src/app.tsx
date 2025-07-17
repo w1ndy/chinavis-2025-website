027b83da21013f533c286841670e92582501070c
Di Weng <dweng.cs@outlook.com>
2025-07-17 17:07:28 +0800

    try pwa

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { Router } from "@solidjs/router";
-import { MetaProvider } from "@solidjs/meta";
+import { Link, Meta, MetaProvider } from "@solidjs/meta";
 import { FileRoutes } from "@solidjs/start/router";
 
 import "./app.css";
@@ -13,6 +13,8 @@ import { Suspense } from "solid-js";
 export default function App() {
   return (
     <MetaProvider>
+      <Link rel="manifest" href="/2025/manifest.webmanifest"></Link>
+      <Meta name="theme-color" content="#f030ff"></Meta>
       <Router
         root={props => (
           <>
